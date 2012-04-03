59e586ed942b94f973ce2a36861b07a0671cfb71
Brian Waldon <bcwaldon@gmail.com>
2012-04-03 10:01:08 -0700

    Adding image-create action

--- glanceclient/v1/shell.py
+++ glanceclient/v1/shell.py
@@ -25,14 +25,26 @@ def do_image_list(gc, args):
     utils.print_list(images, columns)
 
 
-@utils.arg('id', metavar='<IMAGE_ID>', help='ID of image to describe.')
-def do_image_show(gc, args):
-    """Describe a specific image."""
-    image = gc.images.get(args.id)
-
+def _image_show(image):
     # Flatten image properties dict
     info = copy.deepcopy(image._info)
     for (k, v) in info.pop('properties').iteritems():
         info['Property \'%s\'' % k] = v
 
     utils.print_dict(info)
+
+
+@utils.arg('id', metavar='<IMAGE_ID>', help='ID of image to describe.')
+def do_image_show(gc, args):
+    """Describe a specific image."""
+    image = gc.images.get(args.id)
+    _image_show(image)
+
+
+@utils.arg('--id', metavar='<IMAGE_ID>', help='ID of image to reserve.')
+@utils.arg('--name', metavar='<NAME>', help='Name of image.')
+@utils.arg('--disk_format', metavar='<CONTAINER_FORMAT>', help='Disk format of image.')
+@utils.arg('--container_format', metavar='<DISK_FORMAT>', help='Container format of image.')
+def do_image_create(gc, args):
+    image = gc.images.create(*args)
+    _image_show(image)
