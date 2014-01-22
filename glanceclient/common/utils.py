e5f6dee95d7559fafc0e768366b3ac773ac91a4f
llg8212 <lilinguo@huawei.com>
2014-01-22 11:45:42 +0800

    Using common method 'bool_from_string' from oslo strutils
    Using common method 'bool_from_string' from oslo strutils to replace
    utils.string_to_bool.
    
    partially implements blueprint common-client-library-2
    
    Change-Id: I23924db3000feadcfe823c6cc979ea9752a13fa9

--- glanceclient/common/utils.py
+++ glanceclient/common/utils.py
@@ -48,7 +48,7 @@ def schema_args(schema_getter, omit=[]):
     typemap = {
         'string': str,
         'integer': int,
-        'boolean': string_to_bool,
+        'boolean': strutils.bool_from_string,
         'array': list
     }
 
@@ -174,10 +174,6 @@ def is_authentication_required(f):
     skip the authentication step.
     """
     return getattr(f, 'require_authentication', True)
-
-
-def string_to_bool(arg):
-    return arg.strip().lower() in ('t', 'true', 'yes', '1')
 
 
 def env(*vars, **kwargs):
