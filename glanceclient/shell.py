bda4dd4dac9c774d1d8e393586cbf3fbad112a73
Steve Martinelli <stevemar@ca.ibm.com>
2015-12-27 01:37:24 -0500

    use keystoneclient exceptions instead of oslo-incubator code
    depending on any oslo-incubator code from another project is
    dangerous. keystoneclient makes its exceptions public and it's
    not recommended to use any code from
    keystoneclient.openstack.common.apiclient since it's maintained
    by oslo-incubator.
    
    Change-Id: Ibfd9d364d3199fb485987edef06e1de916e57ee5

--- glanceclient/shell.py
+++ glanceclient/shell.py
@@ -41,7 +41,7 @@ from glanceclient import exc
 from keystoneclient.auth.identity import v2 as v2_auth
 from keystoneclient.auth.identity import v3 as v3_auth
 from keystoneclient import discover
-from keystoneclient.openstack.common.apiclient import exceptions as ks_exc
+from keystoneclient import exceptions as ks_exc
 from keystoneclient import session
 
 osprofiler_profiler = importutils.try_import("osprofiler.profiler")
