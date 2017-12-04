1eb97f877fff6e32f181366e7a72c9fd81a647ca
Brian Rosmaita <rosmaita.fossdev@gmail.com>
2017-12-03 23:24:27 -0500

    Migrate dsvm functional test jobs to project repo
    Zuul3-related changes in master have moved functional test gate
    configuration out of infra/project-config and into the glanceclient
    repository.  This patch backports these changes so that the stable
    branch has functional test gates.
    
    Not a pure cherry-pick, also includes setting SWIFT_HASH, which
    did not have to be done in master.
    
    Co-authored-by: Monty Taylor <mordred@inaugust.com>
    Co-authored-by: Brian Rosmaita <rosmaita.fossdev@gmail.com>
    
    (cherry picked from commit c0e63d977fc077be55a02106403840165b89a349)
    (cherry picked from commit a5985508817e5de73092a339f15ce7f9f701a20f)
    
    Depends-On: I84de60181cb88574e341ff83cd4857cce241f2dd
    Depends-On: I0fecb027e5be6f4d7bb0bf34e59a43543a421f38
    Change-Id: Idaa2f4b920e131320499c9e460ade74df1d5a264

---
+++ tools/fix_ca_bundle.sh
@@ -0,0 +1,37 @@
+# When the functional tests are run in a devstack environment, we
+# need to make sure that the python-requests module installed by
+# tox in the test environment can find the distro-specific CA store
+# where the devstack certs have been installed.
+#
+# assumptions:
+# - devstack is running
+# - the devstack tls-proxy service is running
+#
+# This code based on a function in devstack lib/tls
+function set_ca_bundle {
+    local python_cmd='.tox/functional/bin/python'
+    local capath=$($python_cmd -c $'try:\n from requests import certs\n print (certs.where())\nexcept ImportError: pass')
+    # of course, each distro keeps the CA store in a different location
+    local fedora_CA='/etc/pki/tls/certs/ca-bundle.crt'
+    local ubuntu_CA='/etc/ssl/certs/ca-certificates.crt'
+    local suse_CA='/etc/ssl/ca-bundle.pem'
+
+    # the distro CA is rooted in /etc, so if ours isn't, we need to
+    # change it
+    if [[ ! $capath == "" && ! $capath =~ ^/etc/.* && ! -L $capath ]]; then
+        if [[ -e $fedora_CA ]]; then
+            rm -f $capath
+            ln -s $fedora_CA $capath
+        elif [[ -e $ubuntu_CA ]]; then
+            rm -f $capath
+            ln -s $ubuntu_CA $capath
+        elif [[ -e $suse_CA ]]; then
+            rm -f $capath
+            ln -s $suse_CA $capath
+        else
+            echo "can't set CA bundle, expect tests to fail"
+        fi
+    fi
+}
+
+set_ca_bundle
